820c6921fedd819561c057d46de4dfe9a5fdae35
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2026-04-27 22:19:05 +0300

    Add file upload system

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+import os
+import shutil
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+UPLOAD_DIR = "uploads"
+
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
 @app.get("/")
 def home():
-    return {"message": "Backend is running"}
+    return {"message": "Backend running"}
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "filename": file.filename,
+        "message": "Upload successful"
+    }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,46 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [file, setFile] = useState<File | null>(null);
+  const [message, setMessage] = useState("");
+
+  const uploadFile = async () => {
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const res = await fetch("http://127.0.0.1:8000/upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await res.json();
+    setMessage(data.message);
+  };
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>RAG document project</h1>
-    </div>
+    <main className="p-10">
+      <h1 className="text-3xl font-bold mb-6">Doc Upload</h1>
+
+      <input
+        type="file"
+        onChange={(e) => setFile(e.target.files?.[0] || null)}
+        className="mb-4"
+      />
+
+      <br />
+
+      <button
+        onClick={uploadFile}
+        className="bg-black text-white px-4 py-2 rounded cursor-pointer"
+      >
+        Upload
+      </button>
+
+      <p className="mt-4">{message}</p>
+    </main>
   );
 }
